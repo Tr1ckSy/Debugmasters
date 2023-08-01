1b0da74551caf6124cb5d88d3d5eba312df87a08
Lucas g, Saint <109165110+Saint-dev@users.noreply.github.com>
2023-08-01 01:23:47 -0300

    Add files via upload

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -4,19 +4,19 @@
 
              if (cpf.trim() === '') {
                 alert('Digite um CPF válido antes de consultar.');
-                return; // Não prosseguir com a consulta
+                return;
             }
 
             const api_url = `https://scoot-dev.000webhostapp.com/cpf.php?cpf=${cpf}`;
                const btnConsultar = document.getElementById('btnConsultar');
-    const corOriginal = btnConsultar.style.backgroundColor; // Salva a cor original do botão
-    btnConsultar.style.backgroundColor = 'mediumpurple'; // Define a cor temporária (pode ser outra cor de sua escolha)
+    const corOriginal = btnConsultar.style.backgroundColor; 
+    btnConsultar.style.backgroundColor = 'mediumpurple'; 
 
 
             fetch(api_url)
-                .then(response => response.json()) // Obter a resposta como JSON
+                .then(response => response.json()) 
                 .then(data => {
-                    // Atualizar os elementos <span> dentro das tags <p> com as informações retornadas pela API
+                  
                     document.getElementById('cpfDisplay').textContent = data.cpf;
                     document.getElementById('cnsDisplay').textContent = data.cns;
                     document.getElementById('nomeDisplay').textContent = data.nome;
@@ -37,6 +37,8 @@
                     document.getElementById('municipioDisplay').textContent = data.municipio;
                     document.getElementById('estadoDisplay').textContent = data.estado;
                     document.getElementById('cepDisplay').textContent = data.cep;
+                    document.getElementById('tempo_respostaDisplay').textContent = data.tempo_resposta;
+
                  
                     btnConsultar.style.backgroundColor = corOriginal;
                      
@@ -55,39 +57,30 @@
             const resultado = document.getElementById('resultado');
             const elementosParaCopiar = resultado.children;
 
-            // Cria um elemento temporário (textarea) para copiar o texto para a área de transferência
+            
             const textareaTemporario = document.createElement('textarea');
-            textareaTemporario.style.position = 'fixed'; // Evita que o elemento seja exibido na tela
-            textareaTemporario.style.opacity = 0; // Torna o elemento invisível
+            textareaTemporario.style.position = 'fixed'; 
+            textareaTemporario.style.opacity = 0; 
             document.body.appendChild(textareaTemporario);
 
-            // Concatena o conteúdo de cada elemento filho em uma única string, separando com quebras de linha
+           
             let conteudoParaCopiar = '';
             for (const elemento of elementosParaCopiar) {
                 conteudoParaCopiar += elemento.innerText + '\n';
             }
 
-            // Define o conteúdo do textarea temporário com o conteúdo a ser copiado
+        
             textareaTemporario.value = conteudoParaCopiar;
 
-            // Seleciona todo o texto dentro do textarea temporário
+            
             textareaTemporario.select();
 
-            // Copia o texto para a área de transferência
             document.execCommand('copy');
 
-            // Remove o textarea temporário
+        
             document.body.removeChild(textareaTemporario);
 
-            // Exibe uma mensagem para o usuário
+        
             alert('Conteúdo copiado para a área de transferência.');
         }
-  function aumentarContador() {
-      let contadorAtual = parseInt(localStorage.getItem("contador")) || 0;
-      contadorAtual++;
-      localStorage.setItem("contador", contadorAtual);
-      document.getElementById("contador").textContent = contadorAtual;
-    }
-
-    // Exibir o valor atual do contador no console
-    console.log("Acessos: " + localStorage.getItem("contador"));
+ 
