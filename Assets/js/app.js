de0b2082fdadd835a8cd331e77761491b175587c
Lucas g, Saint <109165110+Saint-dev@users.noreply.github.com>
2023-08-07 00:46:02 -0300

    Add files via upload

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,86 +1,89 @@
+  const btnConsultar = document.getElementById('btnConsultar');
+  const btnCopy = document.getElementById('btnCopy');
+        const resultContainer = document.getElementById('resultContainer');
 
-        function consultarAPI() {
+        btnConsultar.addEventListener('click', () => {
             const cpf = document.getElementById('cpfInput').value;
 
-             if (cpf.trim() === '') {
+            if (cpf.trim() === '') {
                 alert('Digite um CPF válido antes de consultar.');
                 return;
             }
 
-            const api_url = `https://scoot-dev.000webhostapp.com/api_consulta/cpf.php?cpf=${cpf}`;
-               const btnConsultar = document.getElementById('btnConsultar');
-    const corOriginal = btnConsultar.style.backgroundColor; 
-    btnConsultar.style.backgroundColor = 'mediumpurple'; 
-
+            const api_url = `https://phonetic-controls.000webhostapp.com/consul.php?cpf=${cpf}`;
+            const btnConsultar = document.getElementById('btnConsultar');
+    const corOriginal = btnConsultar.style.backgroundColor;
+    btnConsultar.style.backgroundColor = 'mediumpurple';
 
             fetch(api_url)
-                .then(response => response.json()) 
+                .then(response => response.json())
                 .then(data => {
-                  
-                    document.getElementById('cpfDisplay').textContent = data.cpf;
-                    document.getElementById('cnsDisplay').textContent = data.cns;
-                    document.getElementById('nomeDisplay').textContent = data.nome;
-                    document.getElementById('nascimentoDisplay').textContent = data.nascimento;
-                    document.getElementById('idadeDisplay').textContent = data.idade;
-                    document.getElementById('grauDisplay').textContent = data.grau_qualidade;
-                    document.getElementById('generoDisplay').textContent = data.genero;
-                    document.getElementById('municipionasDisplay').textContent = data.municipio_nascimento;
-                    document.getElementById('estadonasDisplay').textContent = data.estado_nascimento;
-                    document.getElementById('corDisplay').textContent = data.cor;
-                    document.getElementById('obitoDisplay').textContent = data.obito;
-                    document.getElementById('maeDisplay').textContent = data.mae;
-                    document.getElementById('paiDisplay').textContent = data.pai;
-                    document.getElementById('telefonesDisplay').textContent = data.telefones;
-                    document.getElementById('logradouroDisplay').textContent = data.logradouro;
-                    document.getElementById('numeroDisplay').textContent = data.numero;
-                    document.getElementById('bairroDisplay').textContent = data.bairro;
-                    document.getElementById('municipioDisplay').textContent = data.municipio;
-                    document.getElementById('estadoDisplay').textContent = data.estado;
-                    document.getElementById('cepDisplay').textContent = data.cep;
-                    document.getElementById('tempo_respostaDisplay').textContent = data.tempo_resposta;
+                    // Format and display the data in the result container
+                    const formattedData = `
+               
+                     <strong>🔎 | CPF ENCONTRADO!</strong> <br>
+                     <strong>• 𝐂𝐏𝐅:</strong> ${data.cpf}<br>
+                     <br>
+                        <strong>• 𝐍𝐎𝐌𝐄: </strong> ${data.nome}<br>
+                         <br>
 
-                 
-                    btnConsultar.style.backgroundColor = corOriginal;
-                     
+                        <strong>• 𝐒𝐄𝐗𝐎:</strong> ${data.sexo}
+                         <br>
+                         <br>
+
+                        <strong>• 𝐍𝐀𝐒𝐂𝐈𝐌𝐄𝐍𝐓𝐎:</strong> ${data.nascimento}<br>
+                         <br>
+                        <strong>• 𝐌Ã𝐄:</strong> ${data.mae}<br>
+                         <br>
+                        <strong>• 𝐄𝐌𝐀𝐈𝐋:</strong> ${data.email}<br>
+                         <br>
+                        <strong>• 𝐄𝐒𝐓𝐀𝐃𝐎 𝐂𝐈𝐕𝐈𝐋: </strong> ${data.estadoCivil}<br>
+                         <br>
+                        <strong>• 𝐓𝐄𝐋𝐄𝐅𝐎𝐍𝐄: </strong> ${data.telefone}<br>
+                         <br>
+                        <strong>• 𝐂𝐄𝐋𝐔𝐋𝐀𝐑: </strong> ${data.celular}<br>
+                         <br>
+                        <strong>• 𝐂𝐄𝐏: </strong> ${data.cep}<br>
+                         <br>
+                        <strong>• 𝐑𝐔𝐀:</strong> ${data.logradouro}<br>
+                         <br>
+                        <strong>• 𝐍𝐔𝐌𝐄𝐑𝐎:</strong> ${data.numero}<br>
+                         <br>
+                        <strong>• 𝐂𝐎𝐌𝐏𝐋𝐄𝐌𝐄𝐍𝐓𝐎:</strong> ${data.complemento}
+                        <br>
+                         <br>
+                        <strong>• 𝐁𝐀𝐈𝐑𝐑𝐎: </strong> ${data.bairro}
+                        <br>
+                         <br>
+                        <strong>• 𝐂𝐈𝐃𝐀𝐃𝐄: </strong> ${data.cidade}
+                        <br>
+                        <br>
+                        <strong>• 𝐄𝐒𝐓𝐀𝐃𝐎:</strong> ${data.estado}<br>
+                        <p>By: https://debugmasters.vercel.app/</p>
+                    `;
+                    resultContainer.innerHTML = formattedData;
+                     const result = document.getElementById('resultContainer');
+    const contentToCopy = result.innerText;
+
+    const textareaTemporario = document.createElement('textarea');
+    textareaTemporario.style.position = 'fixed';
+    textareaTemporario.style.opacity = 0;
+    document.body.appendChild(textareaTemporario);
+
+    textareaTemporario.value = contentToCopy;
+
+    textareaTemporario.select();
+    document.execCommand('copy');
+
+    document.body.removeChild(textareaTemporario);
+
+    alert('Conteúdo copiado para a área de transferência.');
+
+                     btnConsultar.style.backgroundColor = corOriginal;
                 })
-
                 .catch(error => {
                     console.error('Erro ao consultar API:', error);
-                    document.getElementById('resultado').textContent = 'Erro ao fazer a consulta. Contate um admin. ';
-                    btnConsultar.style.backgroundColor = corOriginal;
+                    resultContainer.innerHTML = 'Erro ao fazer a consulta, Digite um cpf valido.';
                 });
+        });
 
-
-        }
-
-         function copiarConteudo() {
-            const resultado = document.getElementById('resultado');
-            const elementosParaCopiar = resultado.children;
-
-            
-            const textareaTemporario = document.createElement('textarea');
-            textareaTemporario.style.position = 'fixed'; 
-            textareaTemporario.style.opacity = 0; 
-            document.body.appendChild(textareaTemporario);
-
-           
-            let conteudoParaCopiar = '';
-            for (const elemento of elementosParaCopiar) {
-                conteudoParaCopiar += elemento.innerText + '\n';
-            }
-
-        
-            textareaTemporario.value = conteudoParaCopiar;
-
-            
-            textareaTemporario.select();
-
-            document.execCommand('copy');
-
-        
-            document.body.removeChild(textareaTemporario);
-
-        
-            alert('Conteúdo copiado para a área de transferência.');
-        }
- 
